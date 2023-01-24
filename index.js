29489fe8bfaea41514402093fcf8fca239492aff
Jose Costa <jccosta@MacBook-Air-de-Jose.local>
2023-01-24 21:04:42 +0000

    created questions array

--- index.js
+++ index.js
@@ -6,34 +6,40 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [{
     type: 'input',
-    name: 'title',
+    name: 'Title',
     message: 'Project name:'
 },
 {
     type: 'input',
-    name: 'description',
+    name: 'Description',
     message: 'Project description:'
 
 },
 {
     type: 'input',
-    name: 'installation',
+    name: 'Installation',
     message: 'Installation instructions:'
 },
 {
     type: 'input',
-    name: 'usage',
+    name: 'Usage',
     message: 'Usage Information:'
 },
 {
     type: 'input',
-    name: 'contributin',
+    name: 'Contributin',
     message: 'Contributing Guidelines:'
 },
 {
     type: 'input',
-    name: 'tests',
-    message: 'Test Instructions'
+    name: 'Tests',
+    message: 'Test Instructions:'
+},
+{
+    type: 'list',
+    name: 'License',
+    message: 'License?',
+    choices:['No license', 'MIT', 'ISC']
 }
 ];
 
@@ -43,10 +49,15 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions)
+    .then((answers) => {
+        console.log(answers)
+        return answers
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 }
 
 // function call to initialize program
 init();
-
-console.log(questions);
